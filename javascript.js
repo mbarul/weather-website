16dbe21b98258e6ed107df8deb437f77c749f008
Marek <marek.barul28@gmail.com>
2020-10-19 20:11:10 +0200

    new lines in js - fetching data

--- javascript.js
+++ javascript.js
@@ -2,6 +2,6 @@ function searching() {
 
     x = document.getElementById("nameCity").value;
     document.getElementById("city").innerHTML = x + " :";
-
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=warsaw&appid=118aa3da3a47e88cef12f69cb6bdcf0b`).then(response => {return response.json();}).then( results => console.log(results));
     return false;
 }
