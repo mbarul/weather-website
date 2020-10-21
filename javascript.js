d85e029728a9d5ba681285395c6f1db3143266e2
Marek <marek.barul28@gmail.com>
2020-10-21 21:00:51 +0200

    fetching of conditions and working on the date

--- javascript.js
+++ javascript.js
@@ -2,6 +2,19 @@ function searching() {
 
     x = document.getElementById("nameCity").value;
     document.getElementById("city").innerHTML = x + " :";
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=warsaw&appid=118aa3da3a47e88cef12f69cb6bdcf0b`).then(response => {return response.json();}).then( results => console.log(results));
-    return false;
+
+const fetchWeather =
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&appid=118aa3da3a47e88cef12f69cb6bdcf0b`);
+fetchWeather.then(response => {
+    return response.json();
+}).then(results => {
+  console.log(results);
+  Celsius.innerHTML = results.main.temp;
+  Conditions.innerHTML = results['weather'][0]['description'];
+  Time.innerHTML = results.timezone;
+});
 }
+let d = new Date();
+let ufc_offset = d.getTimezoneOffset();
+d.setMinutes(d.getMinutes() + ufc_offset);
+console.log(d)
