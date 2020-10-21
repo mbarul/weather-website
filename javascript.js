454bb5210adc995fd866bbc78e68dc9914b7e8cd
kamil <kamilbarul123@gmail.com>
2020-10-21 20:32:33 +0200

    added function for temperature

--- javascript.js
+++ javascript.js
@@ -2,6 +2,15 @@ function searching() {
 
     x = document.getElementById("nameCity").value;
     document.getElementById("city").innerHTML = x + " :";
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&appid=118aa3da3a47e88cef12f69cb6bdcf0b`).then(response =>
+    { return response.json(); }).then(results => {
+        console.log(results);
+
+        Celsius.innerHTML = (Math.round(((results.main.temp - 273.15) * 100)) / 100) ;
+
+        var offset = new Date().getTimezoneOffset();
+        console.log(offset);
+    });
 
     return false;
 }
