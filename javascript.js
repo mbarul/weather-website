57f1aa84169c6c6dd90d29eafefbc133381c082d
kamil <kamilbarul123@gmail.com>
2020-10-21 21:03:47 +0200

    added timezone calcutor and display in index

--- javascript.js
+++ javascript.js
@@ -2,14 +2,15 @@ function searching() {
 
     x = document.getElementById("nameCity").value;
     document.getElementById("city").innerHTML = x + " :";
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&appid=118aa3da3a47e88cef12f69cb6bdcf0b`).then(response =>
-    { return response.json(); }).then(results => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${x}&appid=118aa3da3a47e88cef12f69cb6bdcf0b`).then(response => { return response.json(); }).then(results => {
         console.log(results);
 
-        Celsius.innerHTML = (Math.round(((results.main.temp - 273.15) * 100)) / 100) ;
+        Celsius.innerHTML = (Math.round(((results.main.temp - 273.15) * 100)) / 100);
 
-        var offset = new Date().getTimezoneOffset();
-        console.log(offset);
+        var d = new Date();
+        var UTC = d.getTimezoneOffset()*60;
+        d.setSeconds(results.timezone + UTC);
+        document.getElementById("Date").innerHTML = d.getFullYear() +'.'+ d.getMonth() + '.' + d.getDate() + ' , ' + d.getHours() + ':' + d.getMinutes();
     });
 
     return false;
